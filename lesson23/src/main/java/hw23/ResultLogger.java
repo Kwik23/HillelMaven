ddc3e45372ab944097e9635f444c5732e9c799a4
Kwik23 <iszJX@nYRhqpY2W>
2023-07-27 21:50:29 +0300

    homework 23

---
+++ lesson23/src/main/java/hw23/ResultLogger.java
@@ -0,0 +1,15 @@
+package hw23;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class ResultLogger {
+    private static final String RESULT_LOG_FILE = "result.log";
+
+    public void writeResultLog(String logEntry) throws IOException {
+        try (FileWriter writer = new FileWriter(RESULT_LOG_FILE, true)) {
+            writer.write(logEntry + System.lineSeparator());
+        }
+    }
+}
